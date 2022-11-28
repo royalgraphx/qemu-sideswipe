c9a7bfe4b10dd6ce68f932f65948d2b46d9776f8
RGX <39929362+royalgraphx@users.noreply.github.com>
2022-11-27 20:09:40 -0600

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd build
+../configure --enable-sdl --enable-opengl --enable-virglrenderer --enable-system --enable-modules --audio-drv-list=pa --target-list=x86_64-softmmu --enable-kvm --enable-gtk  --enable-libusb
+make
