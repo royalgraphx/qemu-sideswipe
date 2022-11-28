ea927336a65d0585296c4bbc39c76b4a0a370105
RGX <39929362+royalgraphx@users.noreply.github.com>
2022-11-27 22:16:14 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 sh ../configure --enable-sdl --enable-opengl --enable-virglrenderer --enable-system --enable-modules --audio-drv-list=pa --target-list=x86_64-softmmu --enable-kvm --enable-gtk  --enable-libusb
 make
+cd ..
+zip -r qemu-sideswipe.zip build
